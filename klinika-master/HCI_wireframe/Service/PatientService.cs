ee4e05b5a867871883d7a3616609de9d045bcf4a
Maja Tomic <majatomic98@hotmail.com>
2020-10-29 15:00:34 +0100

    #PatientService

--- klinika-master/HCI_wireframe/Service/PatientService.cs
+++ klinika-master/HCI_wireframe/Service/PatientService.cs
@@ -13,6 +13,7 @@ using HCI_wireframe.Service;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Media;
 using System.Text.RegularExpressions;
 
@@ -43,21 +44,22 @@ namespace Class_diagram.Service
             return false;
         }
 
-        private bool isMedicalIdValid(string medicalIDnumber,PatientUser patient)
-        {
-            List<PatientUser> patients = GetAll();
-            foreach (PatientUser patient2 in patients)
-            {
-                if (patient.ID != patient2.ID)
-                {
-                    if (patient2.MedicalIDnumber.Equals(medicalIDnumber))
-                    {
-                        return false;
-                    }
-                }
-            }
+		 private bool isListOfPatientsEmpty(List<PatientUser> patients)
+		 {
+				if (patients.Count == 0) return true;
+				return false;
+		 }
 
-            return true;
+		private bool isMedicalIdValid(string medicalIDnumber,PatientUser patient)
+      {
+			List<PatientUser> patients = new List<PatientUser>();
+			patients = GetAll();
+			   
+			patients = patients.Where(patient2 => (patient.ID != patient2.ID && patient2.MedicalIDnumber.Equals(medicalIDnumber))).ToList();
+			return isListOfPatientsEmpty(patients);
+				
+				
+            
         }
 
         public override Boolean Update(PatientUser patient)
@@ -79,102 +81,79 @@ namespace Class_diagram.Service
         {
             patientsRepository.Delete(patient.ID);
         }
+
+		  private Boolean compareTimeForAppointment(TimeSpan time, DoctorAppointment appointment)
+		  {
+				TimeSpan time1 = TimeSpan.FromMinutes(15);
+				TimeSpan krajPr = appointment.Time.Add(time1);
+				int result = TimeSpan.Compare(time, appointment.Time);
+				int result1 = TimeSpan.Compare(time, krajPr);
+				if ((result == 1 && result1 == -1) || result == 0) return true;
+				return false;
+		  }
+
         public Boolean doesPatientHaveAnAppointmentAtSpecificTime(TimeSpan time, string date, PatientUser patient)
         {
-           AppointmentController appointmentController = new AppointmentController();
+            AppointmentController appointmentController = new AppointmentController();
             List<DoctorAppointment> listOfAppointments = appointmentController.GetAll();
-            if (listOfAppointments == null)
-            {
-                listOfAppointments = new List<DoctorAppointment>();
-            }
+            if (listOfAppointments == null) listOfAppointments = new List<DoctorAppointment>();
+            
             foreach (DoctorAppointment appointment in listOfAppointments)
             {
-                PatientUser patientOnAppointment = appointment.patient;
-                if (patient.ID == patientOnAppointment.ID)
+                if (patient.ID == appointment.patient.ID && appointment.Date.Equals(date))
                 {
-                    if (appointment.Date.Equals(date))
-                    {
-                        TimeSpan time1 = TimeSpan.FromMinutes(15);
-                        TimeSpan krajPr = appointment.Time.Add(time1);
-                        int result = TimeSpan.Compare(time, appointment.Time);
-                        int result1 = TimeSpan.Compare(time, krajPr);
-                        if ((result == 1 && result1 == -1) || result == 0)
-                        {
-                            return true;
-                        }
-                    }
-                }
+						if (compareTimeForAppointment(time,appointment)) return true;
+					 }
             }
             return false;
-        }
+		  }
 
         public bool doesPatientHaveAnAppointmentAtSpecificPeriod(TimeSpan start, TimeSpan end, string dateToString, PatientUser patient)
         {
             bool zauzet = false;
             AppointmentController appController = new AppointmentController();
-
             List<DoctorAppointment> listaPregleda = appController.GetAll();
+
             foreach (DoctorAppointment dd in listaPregleda)
             {
                 PatientUser dr = dd.patient;
-                if (dr.ID == patient.ID)
+                if (dr.ID == patient.ID && dd.Date.Equals(dateToString))
                 {
-                    if (dd.Date.Equals(dateToString))
-                    {
-                        TimeSpan time1 = TimeSpan.FromMinutes(15);
-                        TimeSpan krajPr = dd.Time.Add(time1);
-                        int result = TimeSpan.Compare(start, dd.Time);
-                        int result1 = TimeSpan.Compare(start, krajPr);
-                        if ((result == 1 && result1 == -1) || result == 0)
-                        {
-                            zauzet = true;
-                        }
-                        int rezultat = TimeSpan.Compare(end, dd.Time);
-                        int rezultat1 = TimeSpan.Compare(end, krajPr);
-                        if ((rezultat == 1 && rezultat1 == -1) || rezultat == 0)
-                        {
-
-                            zauzet = true;
-                        }
-                    }
-
-
-                }
+						zauzet = compareTimeForAppointment(start, dd);
+						if(!zauzet)
+							zauzet = compareTimeForAppointment(end, dd);
+					 }
 
             }
             return zauzet;
         }
 
+		  private Boolean compareTimeForOperation(TimeSpan time, TimeSpan start, TimeSpan end)
+		  {
+				int result = TimeSpan.Compare(time, start);
+				int result1 = TimeSpan.Compare(time, end);
+				if ((result == 1 && result1 == -1) || result == 0)
+				{
+					return true;
+				}
+				return false;
+		  }
         public bool doesPatientHaveAnOperationAtSpecificPeriod(TimeSpan start, TimeSpan end, string dateToString, PatientUser patient)
         {
             bool zauzet = false;
             OperationController operationController = new OperationController();
-
             List<Operation> listOfOperation = operationController.GetAll();
+
             foreach (Operation dd in listOfOperation)
             {
                 PatientUser dr = dd.patient;
-                if (dr.ID == patient.ID)
+                if (dr.ID == patient.ID && dd.Date.Equals(dateToString))
                 {
-                    if (dd.Date.Equals(dateToString))
-                    {
-                        int result = TimeSpan.Compare(start, dd.Start);
-                        int result1 = TimeSpan.Compare(start, dd.End);
-                        if ((result == 1 && result1 == -1) || result == 0)
-                        {
-                            zauzet = true;
-                        }
-                        int rezultat = TimeSpan.Compare(end, dd.Start);
-                        int rezultat1 = TimeSpan.Compare(end, dd.End);
-                        if ((rezultat == 1 && rezultat1 == -1) || rezultat == 0)
-                        {
+							zauzet = compareTimeForOperation(start, dd.Start, dd.End);
+							if (!zauzet)
+								zauzet = compareTimeForOperation(end, dd.Start, dd.End);
 
-
-                            zauzet = true;
-                        }
-                    }
-
-                }
+					 }
             }
             return zauzet;
 
@@ -192,11 +171,8 @@ namespace Class_diagram.Service
             }
             foreach (Operation operation in listOfOperation)
             {
-                PatientUser patientOnOperation = operation.patient;
-                if (patientOnOperation.ID == patient.ID)
-                {
-                    if (operation.Date.Equals(date))
-                    {
+                if (operation.patient.ID == patient.ID && operation.Date.Equals(date))
+                {	
                         result1 = TimeSpan.Compare(operation.Start, time);
                         result2 = TimeSpan.Compare(time, operation.End);
 
@@ -204,10 +180,7 @@ namespace Class_diagram.Service
                         {
                             return true;
                         }
-
-                    }
                 }
-
             }
             return false;
         }
